0dfbf498db8d49cb46e42dfd3f2845453008a447
linshixiong <linsx123@qq.com>
2013-06-02 22:59:51 +0800

    添加server端测试程序

---
+++ server/Server/ClientThread.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class ClientThread
+    {
+        public Socket client = null;
+        int i;
+        public ClientThread(Socket k)
+        {
+            client = k;
+        }
+        public void ClientService()
+        {
+            string data = null;
+            byte[] bytes = new byte[1024];
+            Console.WriteLine("新用户的连接。。。");
+            try
+            {
+                while ((i = client.Receive(bytes)) != 0)
+                {
+                    data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                    Console.WriteLine("收到数据,长度：{0},内容：{1}",bytes.Length, data);
+                    data = data.ToUpper();
+                    bytes = System.Text.Encoding.ASCII.GetBytes("data from server time "+DateTime.Now.ToString());
+                    client.Send(bytes);
+                }
+            }
+            catch (System.Exception exp)
+            {
+                Console.WriteLine(exp.ToString());
+            }
+            client.Close();
+            Console.WriteLine("用户断开连接。。。");
+        }  
+    }
+}
---
+++ server/Server/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(599, 159);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 405);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "服务器";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
