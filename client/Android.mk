3aae0482ed8fc30be83bd8385281e9c26f5e8266
linshixiong <linsx.lin@emdoor.com>
2013-06-17 09:07:49 +0800

    添加ble库

--- client/Android.mk
+++ client/Android.mk
@@ -1,13 +1,19 @@
-ifneq ($(BOARD_USE_DEFAULT_APPINSTALL),false)
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
-
+LOCAL_STATIC_JAVA_LIBRARIES := broadcom
 LOCAL_PACKAGE_NAME := AutoTest
 LOCAL_CERTIFICATE := platform
 WITH_DEXPREOPT := false
 include $(BUILD_PACKAGE)
-endif
+include $(CLEAR_VARS)
+
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := broadcom:libs/com.broadcom.bt.jar
+
+include $(BUILD_MULTI_PREBUILT)
+
+# Use the folloing include to make our test apk.
+include $(call all-makefiles-under,$(LOCAL_PATH))
